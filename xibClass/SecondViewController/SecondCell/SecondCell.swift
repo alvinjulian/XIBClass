575a334342e033fce80a5997604fb7a66b30ca21
Alvin Julian <alvin.julian@outlook.com>
2018-07-30 15:50:17 +0700

    add delegation to function extension in second view controller create secondCell insert secondCell to fill TableView

---
+++ xibClass/SecondViewController/SecondCell/SecondCell.swift
@@ -0,0 +1,53 @@
+//
+//  SecondCell.swift
+//  xibClass
+//
+//  Created by Alvin Julian on 30/07/18.
+//  Copyright © 2018 Alvin Julian. All rights reserved.
+//
+
+import UIKit
+
+protocol secondCellDelegate: class {
+    func increaseQuantityPressed()
+    func decreaseQuantityPressed()
+}
+
+
+class SecondCell: UITableViewCell {
+
+    @IBOutlet weak var imageProduct: UIImageView!
+    @IBOutlet weak var productName: UILabel!
+    @IBOutlet weak var productDesc: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    @IBOutlet weak var btnIncrease: UIButton!
+    @IBOutlet weak var btnDecrease: UIButton!
+    @IBOutlet weak var productQuantity: UILabel!
+    
+    weak var delegate: secondCellDelegate?
+    
+    var qty = 0
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func increaseQuantityPressed(_ sender: Any) {
+        delegate?.increaseQuantityPressed()
+        qty += 1
+        productQuantity.text = String(qty)
+    }
+    
+    @IBAction func decreaseQuantityPressed(_ sender: Any) {
+        delegate?.decreaseQuantityPressed()
+        qty -= 1
+        productQuantity.text = String(qty)
+    }
+}
--- xibClass/SecondViewController/SecondViewController.swift
+++ xibClass/SecondViewController/SecondViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.register(UINib(nibName: "SecondCell", bundle: nil), forCellReuseIdentifier: "globalViewForEveryoneIdentifier")
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,4 +35,44 @@ class SecondViewController: UIViewController {
     }
     */
 
+}
+
+extension SecondViewController: UITableViewDataSource {
+    func numberOfSections (in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+}
+
+extension SecondViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 124
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "globalViewForEveryoneIdentifier", for: indexPath) as! SecondCell
+        
+        //Delegate incvrease and decrease function
+        myCell.delegate = self
+        
+        //Untuk change picture di dalam cellnya
+        if indexPath.row % 2 == 0 {
+            myCell.imageProduct.image = #imageLiteral(resourceName: "accelerometer")
+        } else {
+            myCell.imageProduct.image = #imageLiteral(resourceName: "microphone")
+        }
+        
+        return myCell
+    }
+}
+
+extension SecondViewController: secondCellDelegate {
+    func increaseQuantityPressed() {
+        
+    }
+    func decreaseQuantityPressed() {
+        
+    }
 }
