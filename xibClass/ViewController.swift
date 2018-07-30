923aad54f1372a3b304b4ea59e34f781276a3dd7
Alvin Julian <alvin.julian@outlook.com>
2018-07-30 14:16:36 +0700

    Initial Commit

---
+++ xibClass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xibClass
+//
+//  Created by Alvin Julian on 30/07/18.
+//  Copyright © 2018 Alvin Julian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
